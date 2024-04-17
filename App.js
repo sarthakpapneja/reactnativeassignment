71de5424ec032b918ec1a8d8d17024a0c0a3042d
sarthakpapneja <136746591+sarthakpapneja@users.noreply.github.com>
2024-04-17 23:56:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import Background from './Background';
+import Btn from './Btn';
+
+
+const App = (props) => {
+  return (
+    <Background>
+      <View style={{ marginHorizontal: 40, marginVertical: 100 }}>
+      <Text style={{ color: 'white', fontSize: 64 }}>Sarthak Papneja</Text>
+      <Btn bgColor='blue' textColor='white' btnLabel="Reg. No." />
+      <Btn bgColor='white' textColor='blue' btnLabel="22BCE1360" />
+      </View>
+    </Background>
+  );
+}
+
+export default App;
